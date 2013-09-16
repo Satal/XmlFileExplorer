5d7c27ed20ab3c32b5b77f6685d8a66ac1f9b779
Satal <satalketo@gmail.com>
2013-09-16 06:51:53 +0100

    Addition of method comments for XsdValidator

--- trunk/XmlFileExplorer.App/XsdValidator.cs
+++ trunk/XmlFileExplorer.App/XsdValidator.cs
@@ -18,6 +18,11 @@ namespace XmlFileExplorer
             Schemas = new List<XmlSchema>();
         }
 
+        /// <summary>
+        /// Add a schema to be used during the validation of the XML document
+        /// </summary>
+        /// <param name="schemaFileLocation">The file path for the XSD schema file to be added for validation</param>
+        /// <returns>True if the schema file was successfully loaded, else false (if false, view Errors/Warnings for reason why)</returns>
         public bool AddSchema(string schemaFileLocation)
         {
             // Reset the Error/Warning collections
@@ -41,24 +46,39 @@ namespace XmlFileExplorer
             return isValid;
         }
 
+        /// <summary>
+        /// Perform the XSD validation against the specified XML document
+        /// </summary>
+        /// <param name="xmlLocation">The full file path of the file to be validated</param>
+        /// <returns>True if the XML file conforms to the schemas, else false</returns>
         public bool IsValid(string xmlLocation)
         {
+            if (!File.Exists(xmlLocation))
+            {
+                throw new FileNotFoundException("The specified XML file does not exist", xmlLocation);
+            }
+
             using (var xmlStream = new FileStream(xmlLocation, FileMode.Open))
             {
                 return IsValid(xmlStream);
             }
         }
 
+        /// <summary>
+        /// Perform the XSD validation against the supplied XML stream
+        /// </summary>
+        /// <param name="xmlStream">The XML stream to be validated</param>
+        /// <returns>True is the XML stream conforms to the schemas, else false</returns>
         private bool IsValid(Stream xmlStream)
         {
             // Reset the Error/Warning collections
             Errors = new List<string>();
             Warnings = new List<string>();
 
-            var settings = new XmlReaderSettings()
-                {
-                    ValidationType = ValidationType.Schema
-                };
+            var settings = new XmlReaderSettings
+            {
+                ValidationType = ValidationType.Schema
+            };
             settings.ValidationEventHandler += ValidationEventHandler;
 
             foreach (var xmlSchema in Schemas)
@@ -68,9 +88,7 @@ namespace XmlFileExplorer
 
             var xmlFile = XmlReader.Create(xmlStream, settings);
 
-            while (xmlFile.Read())
-            {
-            }
+            while (xmlFile.Read()) { }
 
             return !Errors.Any() && !Warnings.Any();
         }
